eb1406bc184be7e56cc4de138cf8033109c327c7
hosein <hosein@hoseins-iMac.local>
2022-01-28 20:11:59 +0330

    clear console

--- src/App.tsx
+++ src/App.tsx
@@ -206,7 +206,7 @@ export default defineComponent({
     // console.log(slots);
     return () => (
       <div>
-        <BFormInput placeholder="enter youe name" type="text" v-model={bar.value} onInput={(e: any) => handleInput(e)} onChange={(e: any) => handleChange(e)} />
+        <BFormInput placeholder="enter youe name" type="text"  onInput={(e: any) => handleInput(e)} onChange={(e: any) => handleChange(e)} />
       </div>
     );
   },
