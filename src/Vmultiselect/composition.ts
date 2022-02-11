92cafdc21aef7357c9237d3b3e165e22a90a88d5
hosein <hosein@hoseins-iMac.local>
2022-02-11 21:18:31 +0330

    add some functionality for multiselect compnent

--- src/Vmultiselect/composition.ts
+++ src/Vmultiselect/composition.ts
@@ -87,13 +87,14 @@ const customLabelType = (option: any, label: String | any): any => {
 const emptyArray = () => [];
 
 export default {
-  props:{
+  props: {
     internalSearch: { type: Boolean, default: true },
     options: { type: Array, required: true },
     multiple: { type: Boolean, default: false },
     value: { type: null, default: [] },
     trackBy: { type: String },
     label: { type: String },
+    disabled: { type: Boolean, default: false },
     searchable: { type: Boolean, default: true },
     clearOnSelect: { type: Boolean, default: true },
     hideSelected: { type: Boolean, default: false },
@@ -115,14 +116,14 @@ export default {
     blockKeys: { type: Array, default: typeof emptyArray },
     preserveSearch: { type: Boolean, default: false },
     preselectFirst: { type: Boolean, default: false },
-    maxHeight: { type: Number, default: 300 }
+    maxHeight: { type: Number, default: 300 },
   },
 
-  setup(props:any) {
+  setup(props: any, { slots, attrs, emit }: any) {
     let search = "";
-    const isOpen = ref(false);
-    const preferredOpenDirection = ref("below");
-    const optimizedHeight = ref(props.maxHeight);
+    let isOpen = false;
+    let preferredOpenDirection = "below";
+    let optimizedHeight = props.maxHeight;
 
     const internalValue = computed(() =>
       props.value || props.value === 0
@@ -132,12 +133,6 @@ export default {
         : []
     );
 
-    const flatAndStrip = (options: Array<any>) => {
-      return pipe(
-        flattenOptions(props.groupValues, props.groupLabel),
-        strapiGroups
-      )(options);
-    };
     const optionKeys = computed(() => {
       const options = props.groupValues
         ? flatAndStrip(props.options)
@@ -147,9 +142,19 @@ export default {
       );
     });
 
+    const valueKeys = computed(() => {
+      if (props.trackBy) {
+        return internalValue.value.map(
+          (element: any) => element[props.trackBy]
+        );
+      } else {
+        return internalValue.value;
+      }
+    });
+
     const getValue = () => {
       return props.multiple
-        ? internalValue
+        ? internalValue.value
         : internalValue.value.length === 0
         ? null
         : internalValue.value[0];
@@ -168,11 +173,149 @@ export default {
       )(options);
     };
 
+    const flatAndStrip = (options: Array<any>) => {
+      return pipe(
+        flattenOptions(props.groupValues, props.groupLabel),
+        strapiGroups
+      )(options);
+    };
+
     const updateSearch = (query: string) => {
       search = query;
     };
     const isExistingOption = (query: string) => {
       return !props.options ? false : optionKeys.value.indexOf(query) > -1;
+    };
+
+    const isOptionDisabled = (option: any) => {
+      return !!option.isDisabled;
+    };
+
+    const isSelected = (option: any) => {
+      const opt = props.trackBy ? option[props.trackBy] : option;
+      return valueKeys.value.indexOf(opt) > -1;
+    };
+
+    const getOptionLabel = (option: any) => {
+      if (isEmpty(option)) return "";
+      if (option.isTag) return option.label;
+      if (option.isLabel) return option.groupLabel;
+      let label = props.customLabel(option, props.label);
+      if (isEmpty(label)) return "";
+      return label;
+    };
+
+    const select = (option: any, key: any) => {
+      if (option.isLabel && props.groupSelect) {
+        selectGroup(option);
+        return;
+      }
+      if (
+        props.blockKeys.indexOf(key) !== -1 ||
+        option.isDisabled ||
+        option.isLabel
+      )
+        return;
+
+      if (
+        props.max &&
+        props.multiple &&
+        internalValue.value.length === props.max
+      )
+        return;
+
+      if (key === "Tab" && !(this as any).pointerDirty) return;
+      if (option.isTag) {
+        emit("tag", option.label, props.id);
+        search = "";
+        if (props.closeOnSelect && !props.multiple) deactivate();
+      } else {
+        if (isSelected(option)) {
+          if (key !== "Tab") removeElement(option);
+          return;
+        }
+
+        emit("select", option, props.id);
+
+        if (props.multiple) {
+          emit("input", internalValue.value.concat([option]), props.id);
+        } else {
+          emit("input", option, props.id);
+        }
+        if (props.clearOnSelect) search = "";
+      }
+
+      if (props.closeOnSelect) deactivate();
+    };
+
+    const selectGroup = (selectedGroup: any) => {
+      const group = props.options.find((option: any) => {
+        return option[props.groupLabel] === selectedGroup.groupLabel;
+      });
+
+      if (!group) return;
+
+      if (wholeGroupSelected(group)) {
+        emit("remove", group[props.groupValues], props.id);
+
+        const newValue = internalValue.value.filter(
+          (option: any) => group[props.groupValues].indexOf(option) === -1
+        );
+
+        emit("input", newValue, props.id);
+      } else {
+        const optionsToAdd = group[props.groupValues].filter(
+          (option: any) => !(isOptionDisabled(option) || isSelected(option))
+        );
+
+        emit("select", optionsToAdd, props.id);
+        emit("input", internalValue.value.concat(optionsToAdd), props.id);
+      }
+      if (props.closeOnSelect) deactivate();
+    };
+
+    const wholeGroupSelected = (group: any) => {
+      return group[props.groupValues].every(
+        (option: any) => isSelected(option) || isOptionDisabled(option)
+      );
+    };
+
+    const removeElement = (option: any, shouldClose = true) => {
+      if (props.disabled) return;
+      if (option.isDisabled) return;
+
+      if (!props.allowEmpty && internalValue.value.length <= 1) {
+        deactivate();
+        return;
+      }
+
+      const index =
+        typeof option === "object"
+          ? valueKeys.value.indexOf(option[props.trackBy])
+          : valueKeys.value.indexOf(option);
+
+      emit("remove", option, props.id);
+      if (props.multiple) {
+        const newValue = internalValue.value
+          .slice(0, index)
+          .concat(internalValue.value.slice(index + 1));
+        emit("input", newValue, props.id);
+      } else {
+        emit("input", null, props.id);
+      }
+      if (props.closeOnSelect && shouldClose) deactivate();
+    };
+
+    const deactivate = () => {
+      if (!isOpen) return;
+      isOpen = false;
+      if (props.searchable) {
+        // this.$refs.search && this.$refs.search.blur()
+      } else {
+        emit("blur");
+      }
+      if (!props.preserveSearch) search = "";
+      emit("close", getValue(), props.id);
     };
   },
 };
